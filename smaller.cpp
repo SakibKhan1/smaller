593bebf7f2d8853b17ebb063b72d6fdb3e749b3d
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-12 18:07:12 -0500

    Add files via upload

---
+++ smaller.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Sakib Khan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 1 A
+
+This program will find the smaller of two integers when inputted.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstnum, secondnum;
+    cout << "Input first number: " << endl;
+    cin >> firstnum;
+    cout << "Input second number: " << endl;
+    cin >> secondnum;
+    if(firstnum > secondnum){
+        cout << "The smallest of the two numbers is: " << secondnum << endl;
+    } else{
+            cout << "The smallest of the two numbers is: " << firstnum << endl;
+            }
+
+
+    return 0;
+
+}
